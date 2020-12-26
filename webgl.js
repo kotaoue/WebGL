56337647ef64fe4880fb8383d4bd52b8a9aad38b
kota oue <kota.oue@yappli.co.jp>
2020-12-27 07:17:20 +0900

    Update webgl.js

--- webgl.js
+++ webgl.js
@@ -23,8 +23,11 @@ function main() {
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
+    varying lowp vec4 vColor;
+
     void main() {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
+      vColor = aVertexColor;
     }
   `;
 
